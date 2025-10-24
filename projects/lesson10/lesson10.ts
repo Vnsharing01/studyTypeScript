15906126693fc65366b367fe1b92bb022e4e2ff1
thicmn <thichu08@gmail.com>
2025-10-24 17:47:22 +0700

    lesson10 tìm hiểu boolean type

---
+++ projects/lesson10/lesson10.ts
@@ -0,0 +1,10 @@
+let status1: boolean = true;
+let check1: boolean = false;
+ 
+let milft: boolean = Boolean("TypeScript");
+
+console.log(">>> check status: ", status1);
+
+///TODO: boolean !=  Boolean 
+/// boolean là type key
+/// Boolean là 1 object. Nó sẽ tự động chuyển giá trị được truyền vào sang kiểu boolean
